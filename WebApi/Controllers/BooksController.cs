6512250e295a788c17c9719fca3c5555f3c1399d
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2023-09-05 23:29:00 +0300

    lecture -6 finished

---
+++ WebApi/Controllers/BooksController.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Modals;
+using WebApi.Repositories;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly RepositoryContext _context;
+
+        public BooksController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = _context.Books.ToList();
+
+            return Ok(books);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetBook([FromRoute(Name = "id")] int id)
+        {
+            try
+            {
+                var book = _context.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+
+                if (book is null)
+                    return NotFound();
+
+                return Ok(book);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateBook([FromBody] Book book)
+        {
+            try
+            {
+                if (book is null) return BadRequest();
+
+                _context.Books.Add(book);
+                _context.SaveChanges();
+
+                return StatusCode(201, book);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult UpdateBook([FromRoute(Name = "id")] int id, [FromBody] Book book)
+        {
+            try
+            {
+                var entity = _context.Books.Where(x => x.Id.Equals(id)).SingleOrDefault();
+
+                if (entity is null)
+                    return NotFound();
+
+                if (id != book.Id)
+                    return BadRequest();
+
+                entity.Title = book.Title;
+                entity.Price = book.Price;
+
+                _context.SaveChanges();
+
+                return Ok(book);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteBook([FromRoute(Name = "id")] int id)
+        {
+            try
+            {
+                var entity = _context.Books.Where(x => x.Id.Equals(id)).SingleOrDefault();
+
+                if (entity is null)
+                    return NotFound(new
+                    {
+                        statusCode = 404,
+                        message = $"Book with id:{id} could not found."
+                    });
+
+                _context.Books.Remove(entity);
+                _context.SaveChanges();
+
+                return NoContent();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPatch("{id:int}")]
+        public IActionResult PartiallyUpdateBook([FromRoute(Name ="id")] int id, [FromBody] JsonPatchDocument<Book> book)
+        {
+            try
+            {
+                var entity = _context.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+
+                if (entity is null) return NotFound(); // 404
+
+                book.ApplyTo(entity);
+                _context.SaveChanges();
+
+                return NoContent(); // 204
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,11 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<RepositoryContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
 
 var app = builder.Build();
 
