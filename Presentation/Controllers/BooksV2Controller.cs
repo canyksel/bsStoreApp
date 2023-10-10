7496e0d869d6f6e575a6ff1c3d8622d073e34b08
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2023-10-10 23:29:54 +0300

    clean up solution

--- Presentation/Controllers/BooksV2Controller.cs
+++ Presentation/Controllers/BooksV2Controller.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Services.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Presentation.Controllers;
 
--- Presentation/Controllers/RootController.cs
+++ Presentation/Controllers/RootController.cs
@@ -1,11 +1,6 @@
 ﻿using Entities.LinkModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Presentation.Controllers;
 
