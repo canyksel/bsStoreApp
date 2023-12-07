159ef38df6d1b757a59444a02acba5ca2d42ef92
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2023-12-07 23:54:51 +0300

    lecture-27 part-2(bonus)

--- Repositories/Contracts/IRepositoryManager.cs
+++ Repositories/Contracts/IRepositoryManager.cs
@@ -3,5 +3,6 @@
 public interface IRepositoryManager
 {
     IBookRepository Book { get; }
+    ICategoryRepository Category { get; }
     Task SaveAsync();
 }
---
+++ Repositories/EFCore/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Contracts;
+
+namespace Repositories.EFCore;
+
+public class CategoryRepository : RepositoryBase<Category>, ICategoryRepository
+{
+    public CategoryRepository(RepositoryContext context) : base(context)
+    {
+    }
+
+    public void CreateOneCategory(Category category) => Create(category);
+
+    public void DeleteOneCategory(Category category) => Delete(category);
+
+    public async Task<IEnumerable<Category>> GetAllCategoriesAsync(bool trackChanges)
+    {
+        return await FindAll(trackChanges)
+            .OrderBy(c => c.CategoryName)
+            .ToListAsync();
+    }
+    public async Task<Category> GetOneCategoryByIdAsync(int id, bool trackChanges) => await FindByCondition(c => c.CategoryId.Equals(id), trackChanges).SingleOrDefaultAsync();
+
+    public void UpdateOneCategory(Category category) => Update(category);
+}
